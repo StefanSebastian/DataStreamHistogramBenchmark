e592e885f858e4d313c2dff29a806873a16bb7f5
StefanSebastian <stefansebii@gmail.com>
2019-04-30 22:48:47 +0300

    Added the 'add' method to optimalstreaminghist algorithm

--- histogram/src/main/java/com/datastreams/histogram/OptimalStreamingHistograms.java
+++ histogram/src/main/java/com/datastreams/histogram/OptimalStreamingHistograms.java
@@ -1,6 +1,7 @@
 package com.datastreams.histogram;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Algorithm from https://amplitude.com/blog/2014/08/06/optimal-streaming-histograms
@@ -9,7 +10,20 @@ import java.util.HashMap;
  */
 public class OptimalStreamingHistograms {
 
-   private HashMap<Double, Integer> bins = new HashMap<>();
+    static class Coord implements Comparable {
+        double x;
+        int y;
+
+        public Coord(double x, int y){
+            this.x = x; this.y = y;
+        }
+
+        public int compareTo(Object other) {
+            return Double.compare(x, ((NumericHistogram.Coord) other).x);
+        }
+    }
+
+   private List<Coord> bins = new ArrayList<>();
 
     public OptimalStreamingHistograms(double minbound, double maxbound) {
         int p1 = -1;
@@ -24,8 +38,30 @@ public class OptimalStreamingHistograms {
         }
 
         for (int p = p1; p <= p2; p++) {
-            bins.put(Math.pow(10, p), 0);
+            bins.add(new Coord(Math.pow(10, p), 0));
         }
+    }
+
+    public void add(double v) {
+        int bin = 0;
+        for(int l=0, r=bins.size(); l < r; ) {
+            bin = (l+r)/2;
+            if (bins.get(bin).x > v) {
+                r = bin;
+            } else {
+                if (bins.get(bin).x < v) {
+                    l = ++bin;
+                } else {
+                    break; // break loop on equal comparator
+                }
+            }
+        }
+        Coord coord = bins.get(bin - 1);
+        coord.y += 1;
+    }
+
+    public void estimate(double a, double b) {
+
     }
 
 }
--- histogram/src/test/java/com/datastreams/histogram/OptimalStreamingHistogramsTest.java
+++ histogram/src/test/java/com/datastreams/histogram/OptimalStreamingHistogramsTest.java
@@ -10,5 +10,11 @@ public class OptimalStreamingHistogramsTest {
     public void binCreation() {
         OptimalStreamingHistograms hist = new OptimalStreamingHistograms(0.5, 124);
         System.out.println(hist);
+
+        hist.add(5);
+        hist.add(112);
+        hist.add(0.7);
+
+        System.out.println(hist);
     }
 }
